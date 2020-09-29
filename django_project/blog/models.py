fc2b01e96be6fa8ef16ebd96f8d7e5866ebb858b
RomeuCarvalhoAntunes <f795750>
2020-09-28 23:01:50 -0300

    Finalizando blog em django

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -9,10 +9,10 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    publish_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.publish_date = timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
